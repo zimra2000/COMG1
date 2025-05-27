5870bfbc3dca96d626e0e25124d468aae8aeab91
zimra2000 <rrfzimra5@gmail.com>
2025-05-27 10:17:15 +0530

    push() method is created

--- Test.java
+++ Test.java
@@ -5,4 +5,13 @@ public class Test{
 	public boolean isFull(){
 		return top == stk.length - 1;
 	}
+	public void push(){
+		if(isFull()){
+			System.out.println("Stack is Overflow");
+		}
+		else{
+		stk[++top] =item;
+		}
+	}
+
 }
