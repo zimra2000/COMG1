2681e176ca2e9cc1032f24871c3fb342d9866fdc
zimra2000 <rrfzimra5@gmail.com>
2025-05-27 10:23:21 +0530

    pop() method is created

--- Test.java
+++ Test.java
@@ -5,4 +5,24 @@ public class Test{
 	public boolean isFull(){
 		return top == stk.length - 1;
 	}
+	public void push(){
+		if(isFull()){
+			System.out.println("Stack is Overflow");
+		}
+		else{
+		stk[++top] =item;
+		}
+	}
+	public int pop(){
+		if(isEmpty(){
+			System.out.println("Stack is Underrflow");
+			return 0;
+		}
+		else{
+			int item = stk[top++};
+			return item;
+		}
+	}
+
+
 }
